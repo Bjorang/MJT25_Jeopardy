4cc0aed69f3433cf16881c35b1f5d8cfffe9ff5e
PeterJanstad <peter.janstad@hotmail.se>
2025-10-21 10:22:24 +0200

    uploading "working" gameplay

--- Gameplay.java
+++ Gameplay.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+
 public class Gameplay {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+        
+      
+        String correctAnswer = "Hej";
+        
+        if (userInput.equals(correctAnswer)){
+            System.out.println("nu lyckades du din jävel");
+         
+
+        }
+        else {
+            System.out.println("nu gick det inte");
+     
+        }
+    sc.close();    
+    }
+
+   
 }
