a0318f8e1772e21f9cbdc9cd52869567844f40e1
PeterJanstad <peter.janstad@hotmail.se>
2025-10-21 14:02:11 +0200

    main working with gameplay

--- Main.java
+++ Main.java
@@ -2,20 +2,9 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+    Gameplay play = new Gameplay();
+    play.playRound();
 
-        UI ui = new UI();
-        ui.printUI();
-
-        Scanner s = new Scanner(System.in);
-        Questions q = new Questions();
-
-        String userIn = "";
-        String[] question = q.getQuestion(userIn);
-
-        for (int i = 0; i < 4; i++) {
-            System.out.println(question[i]);
-            
-        }
         
     }
 }
