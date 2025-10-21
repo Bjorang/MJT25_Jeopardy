3f479ad512a80c59f1721fb4369f58ed642ab0a3
Bjorang <anders.bjorang@iths.se>
2025-10-21 14:26:39 +0200

    Tog bort en ändring för att kunna göra en pull

--- Gameplay.java
+++ Gameplay.java
@@ -27,9 +27,15 @@ public class Gameplay {
                 System.out.println("OMFG DU HADDE RÄTT!!!");
             } else {
                 System.out.println("lol fel!");
+<<<<<<< HEAD
                 
             }
             numberOfRounds++;
+=======
+               
+            }
+             numberOfRounds++;
+>>>>>>> 30c81a8 (Tog bort en ändring för att kunna göra en pull)
         }
     
 
