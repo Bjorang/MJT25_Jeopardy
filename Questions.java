db52697f1992de8895a1b6713421e3fa98b4c311
”Liz-Malm” <Elizabeth.malm@iths.se>
2025-10-21 11:07:39 +0200

    questions test

--- Questions.java
+++ Questions.java
@@ -1,7 +1,21 @@
 public class Questions {
 
-    String a1 = "b";
 
-    
-    
+
+    String[][] questRow01 = new String[6][5];
+
+    public Questions() {
+        questRow01[0][0] = "Djuret som är mest associerat med meme kulturen";
+        questRow01[0][1] = "a. Vad är hunden?";
+        questRow01[0][2] = "b. Vad är katten?";
+        questRow01[0][3] = "c. Vad är ormen?";
+        questRow01[0][4] = "b";
+
+        
+
+
+
+
+
+    }
 }
