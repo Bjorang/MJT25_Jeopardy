bfe869456c1416866f2d2efb3669bcfcff28c4b4
PeterJanstad <peter.janstad@hotmail.se>
2025-10-21 13:26:38 +0200

    gameplay updated

--- Gameplay.java
+++ Gameplay.java
@@ -1,28 +1,37 @@
 import java.util.Scanner;
 
 public class Gameplay {
-    public static void main(String[] args) {
+    public void playRound(){
         Scanner sc = new Scanner(System.in);
         String correctAnswer = "hej";
-
-        String userInput = sc.nextLine().toLowerCase();
-        boolean  isCorrect= userInput.equals(correctAnswer);
-
         int numberOfRounds = 0;
+        
         
         while (numberOfRounds < 3) {
 
+            UI ui = new UI();
+            ui.printUI();
+            System.out.println("Välj vilken fråga du vill svara på genom att skriva bokstav för kolumnen samt siffra ,t.ex. 'A3'");
+
+            String userInput = sc.nextLine();
+            boolean  isCorrect= userInput.equals(correctAnswer);
             if (isCorrect){
                 System.out.println("nu lyckades du din jävel");
+                break;
             }
 
             else {
                 System.out.println("nu gick det inte");
-                break;
+                numberOfRounds++;
             }
+        }
+
+   if (numberOfRounds == 3){
+
+    System.out.println("nu har du slut på antal rundor");
+   }
+   sc.close();
     }
-    sc.close();  
 }
 
   
-}
