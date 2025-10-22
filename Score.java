b4ed17d1fe82a98ea89d69c39282fdc15f0fa9c0
Bjorang <anders.bjorang@iths.se>
2025-10-22 11:53:27 +0200

    Created Score class

---
+++ Score.java
@@ -0,0 +1,8 @@
+public class Score {
+    
+private int totalScore(){
+if question.questRowA
+
+}
+
+}
--- Variables.java
+++ Variables.java
@@ -3,5 +3,5 @@ public class Variables {
     String correctAnswer;
     String userInput;
     int tempScore;
-    
+    int[] storeScore = {100,200,400,600,800,1000}; 
 }
