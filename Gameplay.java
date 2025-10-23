3cd94ec55a39c7c7a70669783311dc0a5d07cc87
”Liz-Malm” <Elizabeth.malm@iths.se>
2025-10-23 11:57:34 +0200

    omgz! awezum version

--- Gameplay.java
+++ Gameplay.java
@@ -1,65 +1,94 @@
 import java.util.Scanner;
-
 public class Gameplay {
-    public  void playRound(){
-        
-        Variables vars = new Variables(); 
+    
+    Scanner s = new Scanner(System.in); 
+    Variables vars = new Variables();
+    
+    public void playRound(){
+         
+        Questions q = new Questions();
         UI ui = new UI();
         
         while (vars.numberOfRounds < 3) {
 
-            ui.printUI();
+        ui.printUI();
 
-            Scanner s = new Scanner(System.in);
-            Questions q = new Questions();
+        userIn(); 
+
+        vars.currentQuestion = q.getQuestion(vars.userIn);
+
+        ui.editUI(vars.userIn);
+
+        vars.answer = ui.printQuestion(vars.currentQuestion, s);
+
+        userAnswer();
+
+        vars.numberOfRounds++;
+
+        }
+        
+        endGame();
+
+        s.close();
+
+    } 
+
+    public void userIn(){
+
+        do {
+            vars.userIn = s.next().toLowerCase();
+            vars.inputOK = inputCheck(vars.userIn);
             
-            System.out.print("Välj en fråga (t.ex. a1 - a6): ");
-        
-                do {
-                    vars.userIn = s.next().toLowerCase();
-                    vars.inputOK = inputCheck(vars.userIn);
-                    
-                    if (vars.inputOK == false) {
-                        System.out.print("Felaktig input! Vänligen försök igen: ");
-                    } else {
-                                
-                    }
-                
-                } while (!vars.inputOK);
-        
-        String[] question = q.getQuestion(vars.userIn);
-
-        ui.editUI(vars.userIn); 
-
-            for (int i = 0; i < 4; i++) {
-                System.out.println(question[i]);
+            if (vars.inputOK == false) {
+                System.out.print("Felaktig input! Vänligen försök igen: ");
             }
+        } while (!vars.inputOK);
 
-        System.out.print("Svar: ");
-        String answer = s.next();
+    }
 
-            if (answer.equals(question[4])) {
+    public void userAnswer(){{
+
+        if (vars.answer.equals(vars.currentQuestion[4])) {
                 System.out.println("OMFG DU HADE RÄTT!!!");
             } else {
-                System.out.println("lol fel!"); 
+                System.out.println("lol fel!");
             }
-            vars.numberOfRounds++;
-            
         }
-    
-            if (vars.numberOfRounds == 3){
-                System.out.println("nu har du slut på antal rundor");
+    }
+
+    public void endGame(){
+        
+        if (vars.numberOfRounds == 3){
+                System.out.println("nu har du slut på antal rundor"); 
         }
     }
 
     public boolean inputCheck(String input) {
 
-        if (input == null || input.length() < 2) {
-        return false;
-    }
-        char category = input.charAt(0);
-        int questionIndex = Character.getNumericValue(input.charAt(1));
-            
-        return !(category < 'a' || category > 'f' || questionIndex < 1 || questionIndex > 6);
+        if (input.length() < 2) {
+            return false;
+        }
+
+    char category = input.charAt(0);
+    int questionIndex = Character.getNumericValue(input.charAt(1));
+
+    boolean valid = !(category < 'a' || category > 'f' || questionIndex < 1 || questionIndex > 6);
+        
+        if (!valid) {
+            return false;
+        }
+
+        for (int i = 0; i < vars.preIndex; i++) {
+            if (input.equals(vars.questPre[i])) {
+                System.out.println("Denna frågan har du redan valt!");
+                return false;
             }
+        }
+
+    vars.questPre[vars.preIndex] = input;
+    vars.preIndex++;
+
+    return true;
+    }
+
 }
